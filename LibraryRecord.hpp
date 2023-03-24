18598fee2b3b029c6015ac50502e797876639b75
Tsering Gurung <92239331+TseringGurung@users.noreply.github.com>
2023-03-24 19:05:52 -0400

    Update LibraryRecord.hpp

--- LibraryRecord.hpp
+++ LibraryRecord.hpp
@@ -1,117 +1,87 @@
-#include "LibraryRecord.hpp"
-#include "Textbook.hpp"
-#include "Manual.hpp"
-#include "Novel.hpp"
+#ifndef LIBRARY_RECORD_
+#define LIBRARY_RECORD_
 
-/** default constructor**/
-LibraryRecord::LibraryRecord() : ArrayBag<Book*>()
-{
-}
+#include "ArrayBag.hpp"
+#include "Book.hpp"
+#include <vector>
+#include <iostream>
 
-/* parameterized constructor
-Implement a parameterized constructor that takes the name of the input file as a reference to string argument. 
-The constructor will read the input file, where each line corresponds to a Book subclass and 
-dynamically allocate Book-derived objects with the information read from the input file and add them to the LibraryRecord. */
-LibraryRecord::LibraryRecord(const std::string& input_file){
-  
-}
+class LibraryRecord : public ArrayBag<Book*>{
+  public:
+    LibraryRecord(); //default constructor
 
+    /**
+         * @param:  A refrence to a file name (string)
+         * @post:   will read the input file, where each line corresponds to a Book subclass and dynamically allocate Book-derived
+         *          objects with the information read from the input file and add them to the LibraryRecord.
+         * @note:   Input data will be in the following format:
+         *          title, author, page_count, is_digital, genre, subject, grade_level, has_film_adaptation, has_review_questions, device_model, website, book_type
+        */
+    LibraryRecord(const std::string& input_file);
 
+    /** @param:   A reference to a Book object to be checked in
+      @return:  returns true if a book was successfully added to items, false otherwise
+      @post:    adds book to items.
+    **/
+	  bool checkIn(Book* new_entry);
+    
+    /** @param:   A reference to a Book object to be checked out   
+        @return:  returns true if a book was successfully removed from items_, false otherwise
+        @post:    removes the book from the LibraryRecord and if remove was successful, it adds the book to the check_out_history_ vector.
+    **/
+	  bool checkOut(Book* an_entry);
 
+    /**
+      @param:   A reference to a Book object
+      @return:  The number of times (int) the referenced Book has been checked out
+    */
+    int getCheckOutHistory(Book* an_entry);
 
+    /**
+      @return item_count_ : the current size of the bag
+    **/
 
-/** @param:   A reference to a Book object to be checked in
-    @return:  returns true if a book was successfully added to items, false otherwise
-    @post:    adds book to items.
- **/
-bool LibraryRecord::checkIn(Book* new_entry)
-{
-  if(add(new_entry)){
-    return true;
-  }
-  return false;
-}
+    
+    std::vector<Book*> toVector();
 
-/** @param:   A reference to a Book object to be checked out   
-    @return:  returns true if a book was successfully removed from items_, false otherwise
-    @post:    removes the book from the LibraryRecord and if remove was successful, it adds the book to the check_out_history_ vector.
- **/
-bool LibraryRecord::checkOut(Book* an_entry)
-{
-  if(remove(an_entry)) {
-      check_out_history_.push_back(an_entry);
-      return true;
-  }
-  return false;
-}
-
-/**
+    /**
     @post:    Outputs the names of the Books in the LibraryRecord and the number of times each Book has been checked out
 
-      For each Book in the LibraryRecord you will output:
-             [title_] is written by [author_]. Page Count: [page_count_]. [it is / is not] available digitally.\n
-             It has been checked out [_] times.
-**/
-void LibraryRecord::display()
-{
-  for (int i = 0; i < item_count_; i++)
-  {
-    int count = getCheckOutHistory(items_[i]);
-    items_[i]->display();
-    std::cout << "It has been checked out " << count << " times." << std::endl;
-  }
-}
+        For each Book in the LibraryRecord you will output:
+        [title_] is written by [author_]. Page Count: [page_count_]. [it is / is not] available digitally.\n
+        It has been checked out [_] times.
+    **/
+    void display();
 
-/*Implement LibraryRecord::displayFilter that takes a reference to string key and displays information of its holdings
- whenever they key matches the relevant information (specific to the type of book).*/
+    /**
+      @post:    Prints the title of each Book in the LibraryRecord
+      Example:
+      "title1, title2, title3, title4!" Note the commas and exclamation mark.
+    */  
+    void displayTitles();
 
-void LibraryRecord::dispalyFilter(const std::string &key){
-  for(int i = 0; i < item_count_; i++){
-    items_[i]->displayFilter(key);
-  }
-} 
+    /**
+      @return:    Returns true if the LibraryRecord was successfully duplicated, false otherwise (there is nothing to duplicate or duplicating would exceed DEFAULT_CAPACITY).
+      @post:      Duplicates all the items in the LibraryRecord
+      Example: we originally have [book1, book2] and after duplication we have [book1, book2, book1, book2]
+    */
+    bool duplicateStock();
 
-/**
-  @post:    Prints the title of each Book in the LibraryRecord
-  Example:
-  "title1, title2, title3, title4!" Note the commas and exclamation mark.
-*/
-void LibraryRecord::displayTitles()
-{
-  for (int i = 0; i < item_count_; i++)
-  {
-    if (i < item_count_ - 1)
-    {
-      std::cout << items_[i]->getTitle() << ", ";
-    }
-    else
-    {
-      std::cout << items_[i]->getTitle() << "!\n";
-    }
-  }
-}
+    /**
+      @param:   A reference to a book
+      @return: True or false depending on whether or not an item was removed
+      @post: remove all elements of that book
+    */
+    bool removeStock(Book* an_entry);
 
-/**
-  @param:   A reference to a Book object
-  @return:  The number of times (int) the referenced Book has been checked out
-*/
-int LibraryRecord::getCheckOutHistory(Book* an_entry)
-{
-  int count = 0;
+    /**
+      @param:   A reference to another LibraryRecord
+      @return:  Returns true if the 2 library records have the contents, regardless of their order. For example, if the current holdings of the LibraryRecord are [book1, book2, book3]
+      and those of the referenced LibraryRecord are [book3, book1, book2], it will return true
+    */
+    bool equivalentRecords(LibraryRecord& a_library_record);
 
-  for (int j = 0; j < check_out_history_.size(); j++)
-  {
-    if (check_out_history_[j] == an_entry)
-    {
-      count++;
-    }
-  }
-  return count;
-}
-
-
-
-/** @param:   A reference to another ArrayBag object
+    /** @param:   A reference to another ArrayBag object
       @post:    Combines the contents from both ArrayBag objects, EXCLUDING duplicates.
       Example: [book1, book2, book3] /= [book1, book4] will produce [book1, book2, book3, book4]
 
@@ -119,35 +89,9 @@ int LibraryRecord::getCheckOutHistory(Book* an_entry)
       If book4 is in LibraryRecord2 and has been checked out 2 times then it should still be checked out 2 times in LibraryRecord2
       Hint: use getCheckOutHistory and your checkOutHistory_ vector
     */
-void LibraryRecord::operator/=(LibraryRecord& a_library_record)
-{
-  int index = 0;
-  int itemsToAdd = a_library_record.item_count_;
-  while (itemsToAdd > 0)
-  {
-    if (this->item_count_ == DEFAULT_CAPACITY)
-    {
-      break;
-    }
-    if (contains(a_library_record.items_[index]))
-    {
-      break;
-    }
-    this->checkIn(a_library_record.items_[index]);
-    int counter = a_library_record.getCheckOutHistory(a_library_record.items_[index]);
-    for (int i = 0; i < counter; i++)
-    {
-      check_out_history_.push_back(a_library_record.items_[index]);
-    }
-    index++;
-    itemsToAdd--;
-  }
-}
+    void operator/=(LibraryRecord& a_library_record);
 
-
-
-
- /**
+    /**
         @param:   A reference to another LibraryRecord object
         @post:    Combines the contents from both LibraryRecord objects, including duplicates.
         Example: [book1, book2, book3] += [book1, book4] will produce [book1, book2, book3, book1, book4]
@@ -156,121 +100,19 @@ void LibraryRecord::operator/=(LibraryRecord& a_library_record)
         If book4 is in LibraryRecord2 and has been checked out 2 times then it should still be checked out 2 times in LibraryRecord2
         Hint: use getCheckOutHistory and your checkOutHistory_ vector
     */
-void LibraryRecord::operator+=(LibraryRecord& a_library_record)
-{
-  int index = 0;
-  int itemsToAdd = a_library_record.item_count_;
-  while (itemsToAdd > 0)
-  {
-    if (this->item_count_ == DEFAULT_CAPACITY)
-    {
-      break;
-    }
-    this->checkIn(a_library_record.items_[index]);
-    int counter = a_library_record.getCheckOutHistory(a_library_record.items_[index]);
-    for (int i = 0; i < counter; i++)
-    {
-      check_out_history_.push_back(a_library_record.items_[index]);
-    }
-    index++;
-    itemsToAdd--;
-  }
-}
+    void operator+=(LibraryRecord& a_library_record);
 
+     /**
+         * @param:  refrence to a key (string)
+         * @post:   displays information of its holdings whenever they key matches the relevant 
+         *          information (specific to the type of book).
+        */
+    void dispalyFilter(const std::string &key);
 
- /**
-        @param:   A reference to another LibraryRecord object
-        @post:    Combines the contents from both LibraryRecord objects, including duplicates.
-        Example: [book1, book2, book3] += [book1, book4] will produce [book1, book2, book3, book1, book4]
+  protected:
+   
+    std::vector<Book*> check_out_history_; //a list of all the items that have been checked out
 
-        IMPORTANT: We are carrying over the amount of times a book has been checked out.
-        If book4 is in LibraryRecord2 and has been checked out 2 times then it should still be checked out 2 times in LibraryRecord2
-        Hint: use getCheckOutHistory and your checkOutHistory_ vector
-    */
-std::vector<Book*> LibraryRecord::toVector()
-{
-  std::vector<Book*> bag_contents;
-  for (int i = 0; i < item_count_; i++)
-    bag_contents.push_back(items_[i]);
-
-  return bag_contents;
-}
-
-/**
-  @return:    Returns true if the LibraryRecord was successfully duplicated, false otherwise (there is nothing to duplicate or duplicating would exceed DEFAULT_CAPACITY).
-  @post:      Duplicates all the items in the LibraryRecord
-  Example: we originally have [book1, book2] and after duplication we have [book1, book2, book1, book2]
-*/
-bool LibraryRecord::duplicateStock()
-{
-  if (item_count_ * 2 > DEFAULT_CAPACITY)
-  {
-    return false;
-  }
-
-  for (int i = 0; i < item_count_; i++)
-  {
-    items_[i + item_count_] = items_[i];
-  }
-  item_count_ = item_count_ * 2;
-  return true;
-}
-
-/**
-  @param:   A reference to a book
-  @return: True or false depending on whether or not an item was removed
-  @post: remove all elements of that book
-*/
-bool LibraryRecord::removeStock(Book* an_entry)
-{
-  while(remove(an_entry));
-  return true;
-}
-
-/**
-  @param:   A reference to another LibraryRecord
-  @return:  Returns true if the 2 library records have the contents, regardless of their order. 
-            For example, if the current holdings of the LibraryRecord are [book1, book2, book3]
-            and those of the referenced LibraryRecord are [book3, book1, book2], it will return true
-*/
-bool LibraryRecord::equivalentRecords(LibraryRecord& a_library_record)
-{
-  bool result = true;
-
-  //copy contents to count duplicates by marking them as empty
-  // use copy because should not modify items_
-  std::vector<Book*> library_copy = toVector();
-  std::vector<Book*> other_copy = a_library_record.toVector();
-  Book* empty;
-
-  for (int i = 0; i < library_copy.size(); i++)
-  {
-    for (int j = 0; j < other_copy.size(); j++)
-    {
-      if (other_copy[j] != empty && other_copy[j] == library_copy[i])
-      {
-        library_copy[i] = empty;
-        other_copy[j] = empty;
-      }
-    }
-  }
-  //check that all entries have been accounted for
-  // i.e. there are no non-empty left
-  for (int i = 0; i < item_count_; i++)
-  {
-    if (library_copy[i] != empty)
-    {
-      result = false;
-    }
-  }
-  //check that all entries have been accounted for
-  // i.e. there are no non-empty left
-  for (int i = 0; i < a_library_record.item_count_; i++)
-  {
-    if (other_copy[i] != empty)
-    {
-      result = false;
-    }
-  }
-  return result;
-}
+ 
+};
+#endif
