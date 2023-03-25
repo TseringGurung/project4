b03c635fc08f71e524dd4780a063c3261f75da10
Tsering Gurung <92239331+TseringGurung@users.noreply.github.com>
2023-03-24 22:47:27 -0400

    Update Textbook.cpp

--- Textbook.cpp
+++ Textbook.cpp
@@ -1,105 +1,126 @@
 //Tsering Gurung
-//CS 235
+//3/24/23
+//Textbook.cpp
 //Project 4: Polymorphism
+
 #include "Textbook.hpp"
 
-/**
- * Parameterized constructor.
- * @param     : The title of the book (a string)
- * @param     : The author of the book (a string)
- * @param     : The number of pages in the book (a positive integer)
- * @param     : The subject of the book (a string)
- * @param     : A flag indicating whether the book is in digital form
- *              with DEFAULT VALUE False (a Boolean)
- * @param     : The grade level of the book (of type grade_level)
- *              with DEFAULT VALUE NONE
- * @param     : A boolean to indicate whether the book has review
- *              questions with DEFAULT VALUE False
- * @post      : The private members are set to the values of the
- *              corresponding parameters.
-*/
-Textbook::Textbook(std::string title, std::string author, int page_count, std::string subject, bool is_digital, grade_level level, bool has_review_questions) : 
-    Book(title, author, page_count, is_digital),
-    subject_(subject),
-    level_(level),
-    has_review_questions_(has_review_questions) {   }
 
 /**
- * @param  : a reference to a string representing the the subject of the book
- * @post   : sets the private member variable to the value of the parameter
+  Default constructor.
+  Default-initializes all private members. 
+  Booleans are default-initialized to False
 */
-void Textbook::setSubject(const std::string& subject) {
-    subject_ = subject;
+Textbook::Textbook(): subject_{""}, grade_level_{NONE}, has_review_questions_{false}
+{    
 }
 
 /**
- * @return  : the subject of the textbook
+  Parameterized constructor.
+  @param     : The title of the book (a string)
+  @param     : The author of the book (a string)
+  @param     : The number of pages in the book (a positive integer)
+  @param     : A flag indicating whether the book is in digital form 
+                with DEFAULT VALUE False (a Boolean)
+  @param     : The subject of the book (a string)
+  @param     : The grade level of the book (of type grade_level) 
+                with DEFAULT VALUE NONE
+  @param     : A boolean to indicate whether the book has review 
+                questions with DEFAULT VALUE False
+  @post      : The private members are set to the values of the 
+                corresponding parameters.
 */
-std::string Textbook::getSubject() const {
-    return subject_;
+Textbook::Textbook(const std::string& name, const std::string& author, int page_count, const std::string& subject, bool is_digital, grade_level grade, bool has_review):Book(name, author, page_count, is_digital), subject_{subject}, grade_level_{grade}, has_review_questions_{has_review}{
+}
+
+
+/**
+  @param  : a reference to a string representing the the subject of the book
+  @post   : sets the private member variable to the value of the parameter
+**/
+void Textbook::setSubject(const std::string& subject){
+  subject_ = subject;
+}
+
+
+/**
+  @return  : the subject of the textbook
+**/
+std::string Textbook::getSubject() const
+{
+  return subject_;
+} 
+
+/**
+  @param  : a reference to a grade_level
+  @post   : sets the private member variable to the value of the parameter
+**/
+void Textbook::setGradeLevel(const grade_level& level){
+  if(level > NONE && level <= COLLEGE){
+      grade_level_ = level;
+  }
+  else{
+    grade_level_ = NONE;
+  }
 }
 
 /**
- * @param  : a reference to a grade_level
- * @post   : sets the private member variable to the value of the parameter
-*/
-void Textbook::setGradeLevel(const grade_level& level) {
-    level_ = level;
+  @return  : a string representative of the grade_level 
+            (e.g. if the grade level is JUNIOR_HIGH, 
+            it returns "JUNIOR HIGH")
+**/
+std::string Textbook::getGradeLevel() const
+{
+  if(grade_level_ == NONE){
+    return "NONE";
+  }
+  else if(grade_level_ == ELEMENTARY){
+    return "ELEMENTARY";
+  }
+  else if(grade_level_ == JUNIOR_HIGH){
+    return "JUNIOR HIGH";
+  }
+  else if (grade_level_ == HIGH_SCHOOL){
+    return "HIGH SCHOOL";
+  }
+  else {
+    return "COLLEGE";
+  }
 }
 
 /**
- * @return  : a string representative of the grade_level
- *              (e.g. if the grade level is JUNIOR_HIGH,
- *              it returns "JUNIOR HIGH")
-*/
-std::string Textbook::getGradeLevel() const {
-    if(level_ == NONE)        return "NONE";
-    if(level_ == ELEMENTARY)  return "ELEMENTARY";
-    if(level_ == JUNIOR_HIGH) return "JUNIOR HIGH";
-    if(level_ == HIGH_SCHOOL) return "HIGH SCHOOL";
-    if(level_ == COLLEGE)     return "COLLEGE";
-    
-    return "If you get here, we got a problem";
+  @post   : sets the review question flag to True
+**/
+void Textbook::setReviewQuestions(){
+  has_review_questions_ = true;
 }
 
 /**
- * @post   : sets the review question flag to True
-*/
-void Textbook::setReviewQuestions() {
-    has_review_questions_ = true;
+  @return  : the flag indicating if the texbook has review questions
+**/
+bool Textbook::checkReviewQuestions() const
+{
+  return has_review_questions_;
 }
 
 /**
- * @return  : the flag indicating if the texbook has review questions
-*/
-bool Textbook::checkReviewQuestions() const {
-    return has_review_questions_;
-}
-
-/**
- * @post     : displays Textbook data in the form:
- * "[title_] is written by [author_] for [grade_level] students. Subject: [subject]. [It has/ It does not have] review questions. Page Count: [page_count_]. [It is / It is not] available digitally.\n" 
+  @post     : displays Textbook data in the form:
+              "[title_] is written by [author_] for [grade_level_] students. 
+              Subject: [subject_]. [It has/ It does not have] review questions. Page Count: [page_count_]. [It is / It is not] available digitally.\n" 
 */ 
-void Textbook::display() const {
-    std::cout << getTitle() << " is written by " << getAuthor() << " for " << getGradeLevel() << " students. Subject: " << getSubject() << ". " << (checkReviewQuestions() ? "It has" : "It does not have") << " review questions. " << "Page Count: " << getPageCount() << ". " << (isDigital() ? "It is" : "It is not") << " available digitally.\n";
+void Textbook::display(){
+  std::cout << getTitle() << " is written by " << getAuthor() << " for " << getGradeLevel() << " students. Subject: " << getSubject() << ". " << (checkReviewQuestions() ? "It has" : "It does not have") << " review questions. Page Count: " << getPageCount() << ". " << (isDigital() ? "It is" : "It is not") << " available digitally.\n";
 }
 
 /**
- * @param    : a reference to a string key to match the subject of the Textbook
- * @post     : calls display() if the key argument is equal to subject_ (e.g. "mathematics")   
+  @param    : a reference to a string key to match the subject of the Textbook
+  @post     : calls display() if the key argument is equal to subject_ (e.g. "mathematics")   
 */
-void Textbook::displayFilter(const std::string& key) const {
-    if(key == subject_) display();
+void Textbook::displayFilter(const std::string &key){
+  if(key == getSubject()){
+    display();
+  }
 }
 
-/**
- * @param   : refrence to (string) grade_level.
- * @return  : returns (enum) grade_level.
-*/
-grade_level stringToGradeLevel(const std::string& input) {
-    if(input == "ELEMENTARY")   return ELEMENTARY;
-    if(input == "JUNIOR_HIGH")  return JUNIOR_HIGH;
-    if(input == "HIGH_SCHOOL")  return HIGH_SCHOOL;
-    if(input == "COLLEGE")      return COLLEGE;
-    return NONE;
-}
+
+
