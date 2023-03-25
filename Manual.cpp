0970a0fb784855e0401586250375620aa9609d52
Tsering Gurung <92239331+TseringGurung@users.noreply.github.com>
2023-03-24 22:35:55 -0400

    Update Manual.cpp

--- Manual.cpp
+++ Manual.cpp
@@ -1,7 +1,4 @@
-#ifndef MANUAL_HPP_
-#define MANUAL_HPP_
-
-#include "Book.hpp"
+#include "Manual.hpp"
 
 /**
  * @param   : a website url
@@ -10,102 +7,130 @@
  *              or 'http://www.something.something' with the last 
  *              'something' being at least 2 characters
 */
-bool isValidURL(std::string url);
+bool isValidURL(std::string url) {
+    if(url.substr(0, 12) == "https://www.") url = url.substr(12);
+    else if(url.substr(0, 11) == "http://www.") url = url.substr(11);
+    else return false;
 
-class Manual : public Book {
-    private:
-        std::string website_url_;   // a string that represents a url.
-        std::string device_name_;   // a string that represents the name of the device.
-        bool has_visual_aid_;       // a boolean indicating whether the Manual has a visual aid.
-        bool has_website_;          // a boolean indicating whether the Manual has a website.
+    if(url.find_first_of('.') != 0) url = url.substr(url.find_first_of('.') + 1);
+    else return false;
 
-    public:
-        /**
-         * Default constructor.
-         * Default-initializes all private members.
-        */
-        Manual()=default;
+    if(url.length() < 2) return false;
 
-        /**
-         * Parameterized constructor.
-         * @param     : The title of the book (a string)
-         * @param     : The author of the book (a string)
-         * @param     : The number of pages in the book (a positive integer)
-         * @param     : The name of the device (a string)
-         * @param     : A flag indicating whether the book is in digital form
-         *               with DEFAULT VALUE False (a Boolean)
-         * @param     : The URL that is in the format
-         *              'https://www.something.something'
-         *               or 'http://www.something.something'
-         *               with the last 'something' being at least 2 characters
-         *               with DEFAULT VALUE empty string (a string)
-         * @param     : A Boolean indicating the presence of a visual aid
-         *               with DEFAULT VALUE False
-         * @post      : The private members are set to the values of the
-         *               corresponding parameters.
-         *               If a URL is provided, the website flag is set to True.
-         *               If the URL is ill-formatted, the website is set to
-         *               empty string and the website flag is set to False.
-        */
-        Manual(std::string title, std::string author, int page_count, std::string device_name, bool is_digital=false, std::string website_url="", bool has_visual_aid=false);
+    return true;
+}
 
-        /**
-            * @param    : a refrence to a string representing the device.
-            * @post     : sets the private member variable to the value of the parameter.
-        */
-        void setDevice(const std::string& device);
+/**
+ * Parameterized constructor.
+ * @param     : The title of the book (a string)
+ * @param     : The author of the book (a string)
+ * @param     : The number of pages in the book (a positive integer)
+ * @param     : The name of the device (a string)
+ * @param     : A flag indicating whether the book is in digital form
+ *               with DEFAULT VALUE False (a Boolean)
+ * @param     : The URL that is in the format
+ *              'https://www.something.something'
+ *               or 'http://www.something.something'
+ *               with the last 'something' being at least 2 characters
+ *               with DEFAULT VALUE empty string (a string)
+ * @param     : A Boolean indicating the presence of a visual aid
+ *               with DEFAULT VALUE False
+ * @post      : The private members are set to the values of the
+ *               corresponding parameters.
+ *               If a URL is provided, the website flag is set to True.
+ *               If the URL is ill-formatted, the website is set to
+ *               empty string and the website flag is set to False.
+*/
+Manual::Manual(std::string title, std::string author, int page_count, std::string device_name, bool is_digital, std::string website_url, bool has_visual_aid) :
+    Book(title, author, page_count, is_digital),
+    device_name_(device_name),
+    website_url_(website_url),
+    has_visual_aid_(has_visual_aid) {
+        if(website_url != "" && isValidURL(website_url))
+            setWebsite(website_url);
+        else {
+            website_url_ = ""; 
+            has_website_ = false;
+        }
+}
 
-        /**
-         * @return    : the variable indicated the device the manual is for.
-         */
-        std::string getDevice() const;
+/**
+    * @param    : a refrence to a string representing the device.
+    * @post     : sets the private member variable to the value of the parameter.
+*/
+void Manual::setDevice(const std::string& device){
+    device_name_ = device;
+}
 
-        /**
-         * @param  : a reference to a website link (string) that is in the format
-         *             'https://www.something.something' 
-         *              or 'http://www.something.something'
-         *              with the last 'something' being at least 2 characters.
-         * @return : has_website_ flag (a boolean)
-         * @post   : If the link is not correctly formatted,
-         *             store "Broken Link" in the link member variable (see <regex>)
-         *             and either way set the has website flag to True
-         */
-        bool setWebsite(const std::string& website_link);
+/**
+ * @return    : the variable indicated the device the manual is for.
+ */
+std::string Manual::getDevice() const {
+    return device_name_;
+}
 
-        /**
-         * @return  : the url for the website.
-        */
-        std::string getWebsite() const;
+/**
+ * @param  : a reference to a website link (string) that is in the format
+ *             'https://www.something.something' 
+ *              or 'http://www.something.something'
+ *              with the last 'something' being at least 2 characters.
+ * @return : has_website_ flag (a boolean)
+ * @post   : If the link is not correctly formatted,
+ *             store "Broken Link" in the link member variable (see <regex>)
+ *             and either way set the has website flag to True
+ */
+bool Manual::setWebsite(const std::string& website_link) {
+    if(!isValidURL(website_link)) website_url_ = "Broken Link";
+    else website_url_ = website_link;
 
-        /**
-         * @param   : a boolean
-         * @post    : sets the private member variable indicating the presence
-         *              of a visual aid to the value of the parameter.
-        */
-        void setVisualAid(const bool& has_visual_aid);
+    has_website_ = true;
 
-        /**
-         * @return  : the visual aid flag.
-        */
-        bool hasVisualAid() const;
+    return has_website_;
+}
 
-        /**
-         * @return  : the has website flag.
-        */
-        bool hasWebsite() const;
+/**
+ * @return  : the url for the website.
+*/
+std::string Manual::getWebsite() const {
+    return website_url_;
+}
 
-        /**
-         * @post     : displays Manual data in the form:
-         * "[title_] is written by [author] company for device: [device]. Website: [website / NONE]. Page Count: [page_count_]. [It is / It is not] available digitally.\n"  
-         */
-        void display() const;
+/**
+ * @param   : a boolean
+ * @post    : sets the private member variable indicating the presence
+ *              of a visual aid to the value of the parameter.
+*/
+void Manual::setVisualAid(const bool& has_visual_aid) {
+    has_visual_aid_ = has_visual_aid;
+}
 
-        /**
-         * @param    : a reference to a string key to match the device of the Manual
-         * @post     : calls display() if the key argument is equal to device_ (e.g. "Oven")   
-         */
-        void displayFilter(const std::string& key) const;
+/**
+ * @return  : the visual aid flag.
+*/
+bool Manual::hasVisualAid() const {
+    return has_visual_aid_;
+}
 
-};
+/**
+ * @return  : the has website flag.
+*/
+bool Manual::hasWebsite() const {
+    return has_website_;
+}
 
-#endif
+/**
+ * @post     : displays Manual data in the form:
+ * "[title_] is written by [author] company for device: [device]. Website: [website / NONE]. Page Count: [page_count_]. [It is / It is not] available digitally.\n"  
+ */
+void Manual::display() const {
+    std::cout << getTitle() << " is written by " << getAuthor() << " company for device: " << getDevice() << ". Website: " << (hasWebsite() ? getWebsite() : "NONE") << ". Page Count: " << getPageCount() << ". " << (isDigital() ? "It is" : "It is not") << " available digitally.\n";
+
+}
+
+/**
+ * @param    : a reference to a string key to match the device of the Manual
+ * @post     : calls display() if the key argument is equal to device_ (e.g. "Oven")   
+ */
+void Manual::displayFilter(const std::string& key) const {
+    if (key == device_name_.substr(0, device_name_.find('-'))) display();
+}
